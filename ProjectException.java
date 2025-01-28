a24ae8182d85194cc860175072f03d4b930d7d05
justinyc1 <justinyijiachen@gmail.com>
2025-01-28 17:09:10 -0500

    organize files

--- ProjectException.java
+++
@@ -1,5 +0,0 @@
-public class ProjectException extends Exception {
-    public ProjectException(String message) {
-        super(message);
-    }
-}
---
+++ javacode/ProjectException.java
@@ -0,0 +1,5 @@
+public class ProjectException extends Exception {
+    public ProjectException(String message) {
+        super(message);
+    }
+}
