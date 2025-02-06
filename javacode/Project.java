a8d5dae44f43d6992d64193abf21314a2e74b486
justinyc1 <justinyijiachen@gmail.com>
2025-02-06 01:45:05 -0500

    Reimplemented and updated MyTuple.java as Tuple.java, updated Project.java to use Tuple.java, refactored for meaningful names, added more comments

--- javacode/Project.java
+++ javacode/Project.java
@@ -2,28 +2,26 @@ package javacode; // **remove this if this file is NOT in a folder called 'javac
 
 //===== libraries that are used for this program =====
 import java.util.Set;
+import java.util.List;
 import java.util.HashSet;
+import java.util.ArrayList;
 
 public class Project {
     public static void main(String[] args) throws ProjectException {
-        // validate_V_set(5, 2);
-        // validate_V_set(9, 2);
-        validate_V_set(15, 2);
+        // validate_set_V(5, 2);
+        // validate_set_V(9, 2);
+        validate_set_V(15, 2);
     }
 
     /**
-     * 
-     * @param m where m is: odd, or equal to p*q (where p and q are different primes), or equal to p^n where n >= 2
-     * @param d where d in the range: 1 <= d <= (m-1)/2
+     * TODO: possibly rename the method to a more meaningful name
+     * @param m - An positve odd integer
+     * @param d - An positive integer in the range: 1 <= d <= (m-1)/2
      */
-    public static void validate_V_set(int m, int d) throws ProjectException {
-        //===== make sure 'm' and 'd' are valid =====
-        if (m % 2 == 0) throw new ProjectException("m is not odd: m = " + m);
-        // to be implemented: code that check and ensures that m = p*q
-        // to be implemented: code that check and ensures that m = p^n where n >= 2
-        if (d < 1) throw new ProjectException("d is not greater than or equal to 1: d = " + d);
-        if (d > (m-1)/2) throw new ProjectException("d is not less than or equal to (m-1)/2: d = " + d + ", (m-1)/2 = " + (m-1)/2);
+    public static void validate_set_V(int m, int d) throws ProjectException {
+        validate_m_and_d(m, d);
 
+        //TODO: extract this section into a method
         //===== create and put valid values in 'Z/mZ' and 'Z/mZ*' set =====
         Set<Integer> Z_mod_m_Z = new HashSet<>();
         Set<Integer> Z_mod_m_Z_star = new HashSet<>();
@@ -34,83 +32,89 @@ public class Project {
             }
         }
 
-        System.out.println(Z_mod_m_Z.toString()); //debug
-        System.out.println(Z_mod_m_Z_star.toString()); //debug
+        // System.out.println(Z_mod_m_Z.toString()); //DEBUG
+        // System.out.println(Z_mod_m_Z_star.toString()); //DEBUG
 
         // deal with B_m^n and U_m^n where n:
         int n = (2 * d) - 2;
         // the U set contains alpha sets, where each alpha set 
         // for set alpha, range of each term: 1 <= a_i <= m-1
-        int alpha_size = n + 2; // a_0, a_1, ... a_n, a_n+1
-        Set<HashSet<Integer>> U_set = new HashSet<>(); // contains all valid alpha sets
+        int alpha_length = n + 2; // a_0, a_1, ... a_n, a_n+1
 
-        // int ZmmZs_alpha_diff = Z_mod_m_Z_star.size() - alpha_size;
-        // int total_possible_combinations = ZmmZs_alpha_diff
+        Set<Tuple<Integer>> U_set = new HashSet<>(); // contains all valid alpha sets
 
-        Set<Integer> alpha = new HashSet<>(); 
+        find_all_valid_alpha_combinations(U_set, Z_mod_m_Z, m, alpha_length);
 
-        // findAllCombinations(Z_mod_m_Z_star, alpha_size); // iteratively
-
-        recursivelyFindAllCombinations(U_set, Z_mod_m_Z, m, alpha_size);
         System.out.println("Print U set: " + U_set);
         System.out.println("U set size:" + U_set.size());
 
-        System.out.println("no errors");
+        System.out.println("method validate_set_V(" + m + ", " + d + ") ran to completion.");
         return;
     }
 
-    public static void findAllCombinations(Set<Integer> Z_mod_m_Z, int alpha_size) {
-        HashSet<Integer> thisSet = new HashSet<>();
+    /** Makes sure m and d are valid 
+     * 
+     * @param m where m is: odd, or equal to p*q (where p and q are different primes), or equal to p^n where n >= 2
+     * @param d where d is in the range: 1 <= d <= (m-1)/2
+     * @throws ProjectException
+     */
+    public static void validate_m_and_d(int m, int d) throws ProjectException {
+        //===== =====
+        if (m % 2 == 0) throw new ProjectException("m is not odd: m = " + m);
+        // TODO: code that check and ensures that m = p*q
+        // TODO: code that check and ensures that m = p^n where n >= 2
+        if (d < 1) throw new ProjectException("d is not greater than or equal to 1: d = " + d);
+        if (d > (m-1)/2) throw new ProjectException("d is not less than or equal to (m-1)/2: d = " + d + ", (m-1)/2 = " + (m-1)/2);
+    }
+
+    /** Calls the recursive algorithm to find all valid ascending combinations of alpha tuple
+     * 
+     * @param U_set - the set that contains all valid alpha tuples
+     * @param Z_mod_m_Z
+     * @param m
+     * @param alpha_length - length of each alpha tuple, determined by n
+     */
+    public static void find_all_valid_alpha_combinations(Set<Tuple<Integer>> U_set, Set<Integer> Z_mod_m_Z, int m, int alpha_length) {
+        Object[] ZmmZ_array = Z_mod_m_Z.toArray();
+        List<Integer> this_combination = new ArrayList<>();
         int sum = 0;
-        Object[] Z_mod_m_Z_array = Z_mod_m_Z.toArray();
-        for (int i = 0; i < Z_mod_m_Z.size() - alpha_size + 1; ++i) {
-            thisSet.add((int)Z_mod_m_Z_array[i]);
-            sum += (int)Z_mod_m_Z_array[i];
-            for (int j = i+1; j < Z_mod_m_Z.size() - alpha_size + 2; ++j) {
-                thisSet.add((int)Z_mod_m_Z_array[j]);
-                sum += (int)Z_mod_m_Z_array[j];
-                for (int k = j+1; k < Z_mod_m_Z.size() - alpha_size + 3; ++k) {
-                    thisSet.add((int)Z_mod_m_Z_array[k]);
-                    sum += (int)Z_mod_m_Z_array[k];
-                    for (int h = k+1; h < Z_mod_m_Z.size() - alpha_size + 4; ++h) {
-                        thisSet.add((int)Z_mod_m_Z_array[h]);
-                        sum += (int)Z_mod_m_Z_array[h];
-                        if (thisSet.size() == alpha_size) System.out.println(thisSet.toString() + "     sum: " + sum + "    i,j,k,h: " + i + "," + j + "," + k + "," + h);
-                        thisSet.remove((int)Z_mod_m_Z_array[h]);
-                        sum -= (int)Z_mod_m_Z_array[h];
-                    }
-                    thisSet.remove((int)Z_mod_m_Z_array[k]);
-                    sum -= (int)Z_mod_m_Z_array[k];
-                }
-                thisSet.remove((int)Z_mod_m_Z_array[j]);
-                sum -= (int)Z_mod_m_Z_array[j];
-            }
-            thisSet.remove((int)Z_mod_m_Z_array[i]);
-            sum -= (int)Z_mod_m_Z_array[i];
+        recursively_find_all(U_set, ZmmZ_array, m, alpha_length, this_combination, sum, 1, 1);
+    }
+
+    /** Recursively find all valid ascending combinations of alpha tuple, using the values of Z/mZ (excluding 0)
+     * 
+     * @param U_set - the set that contains all valid alpha tuples
+     * @param ZmmZ_array - Z/mZ as an array
+     * @param m 
+     * @param alpha_length - length of each alpha tuple, determined by n
+     * @param this_combination - a List that contains all the elements in the alpha tuple for the current loop
+     * @param sum - stores the sum of the elements in this_combination
+     * @param begin - the index of the smallest value that this current position can be
+     * @param depth - the current level of the recursion method
+     */
+    private static void recursively_find_all(Set<Tuple<Integer>> U_set, Object[] ZmmZ_array, int m, int alpha_length, List<Integer> this_combination, int sum, int begin, int depth) {
+        if (depth > alpha_length) return;
+
+        for (int i = begin; i < ZmmZ_array.length - alpha_length + depth; ++i) {
+            this_combination.add((int)ZmmZ_array[i]);
+            sum += (int)ZmmZ_array[i];
+
+            if (this_combination.size() == alpha_length) System.out.println(this_combination.toString() + "   sum: " + sum); //DEBUG
+            if (this_combination.size() == alpha_length && sum % m == 0) U_set.add(new Tuple<Integer>(this_combination));
+            
+            recursively_find_all(U_set, ZmmZ_array, m, alpha_length, this_combination, sum, i+1, depth+1);
+            
+            this_combination.remove((Integer)ZmmZ_array[i]);
+            sum -= (int)ZmmZ_array[i];
         }
     }
 
-    public static void recursivelyFindAllCombinations(Set<HashSet<Integer>> U_set, Set<Integer> Z_mod_m_Z, int m, int alpha_size) {
-        Object[] ZmmZs_array = Z_mod_m_Z.toArray();
-        HashSet<Integer> thisSet = new HashSet<>();
-        int sum = 0;
-        rFAC(U_set, ZmmZs_array, m, alpha_size, thisSet, sum, 1, 1);
-    }
-
-    private static void rFAC(Set<HashSet<Integer>> U_set, Object[] ZmmZs_array, int m, int alpha_size, HashSet<Integer> thisSet, int sum, int begin, int depth) {
-        if (depth > alpha_size) return;
-
-        for (int i = begin; i < ZmmZs_array.length - alpha_size + depth; ++i) {
-            thisSet.add((int)ZmmZs_array[i]);
-            sum += (int)ZmmZs_array[i];
-            if (thisSet.size() == alpha_size) System.out.println(thisSet.toString() + "   sum: " + sum);
-            if (thisSet.size() == alpha_size && sum % m == 0) U_set.add(new HashSet<Integer>(thisSet));
-            rFAC(U_set, ZmmZs_array, m, alpha_size, thisSet, sum, i+1, depth+1);
-            thisSet.remove((int)ZmmZs_array[i]);
-            sum -= (int)ZmmZs_array[i];
-        }
-    }
-
+    /** Return the gcd of a and b using the euclidean algorithm
+     * 
+     * @param a - an integer
+     * @param b - an integer
+     * @return the greatest common divisor of a and b
+     */
     public static int gcd(int a, int b) {
         if (a == 0) return b;
         return gcd(b % a, a);
---
+++ javacode/Tuple.java
@@ -0,0 +1,86 @@
+package javacode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/** Tuple: A collection of elements that is: finite, ordered, immutable, fixed-length, allows repetition, and of type integer
+ */
+public class Tuple<E> {
+    final E[] elements; // final: can't be change once assigned an elements 
+
+    /** Create a Tuple from the contents of a List
+     * 
+     * @param values
+     */
+    @SuppressWarnings("unchecked")
+    public Tuple(List<E> values) {
+        int capacity = values.size();
+        elements = (E[]) new Object[capacity];
+        for (int i = 0; i < capacity; ++i) {
+            elements[i] = values.get(i);
+        }
+    }
+
+    /** Get the capacity/length of the tuple
+     * 
+     * @return the capacity/length of the tuple
+     */
+    public int size() {
+        return elements.length;
+    }
+
+    /** Get an element by its index
+     * 
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        if (index < 0 || index >= elements.length) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return elements[index];
+    }
+
+    /** Print the tuple surrounded by parentheses
+     * 
+     */
+    @Override
+    public String toString() {
+        int lastElementIndex = elements.length-1;
+        StringBuilder sb = new StringBuilder("(");
+        for (int i = 0; i < lastElementIndex; ++i) {
+            sb.append(elements[i]).append(", ");
+        }
+        sb.append(elements[lastElementIndex]).append(")");
+        return sb.toString();
+    }
+
+    /** Converts the tuple into a List
+     * 
+     * @return a List with the same content as the tuple
+     */
+    public List<E> toList() {
+        List<E> newList = new ArrayList<>();
+        for (int i = 0; i < elements.length; ++i) {
+            newList.add(elements[i]);
+        }
+        return newList;
+    }
+
+    /** Compares a tuple to another object and returns true if both tuples are equal
+     * 
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // they are the same object
+        if (obj == null || getClass() != obj.getClass()) return false; // different class
+        Tuple<E> other = (Tuple<E>) obj; // same class, cast obj and compare
+        for (int i = 0; i < elements.length; ++i) {
+            if (!elements[i].equals(other.elements[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
